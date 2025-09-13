fa7f3a806408da15c0632306e726b130c2c8b9db
Tekano Meso <tekanonetflorist01@gmail.com>
2025-09-13 19:41:35 +0200

    Update _layout.tsx

--- prodev-mobile-app-0x05/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x05/app/(home)/_layout.tsx
@@ -1,48 +1,55 @@
 import { Tabs } from "expo-router";
-import { Ionicons } from "@expo/vector-icons";
+import { AntDesign, EvilIcons, Feather, FontAwesome, Ionicons } from "@expo/vector-icons";
 
-export default function HomeLayout() {
+export default function HomeRootLayout() {
   return (
     <Tabs
       screenOptions={{
+        tabBarActiveTintColor: '#34967C',
         headerShown: false,
-        tabBarActiveTintColor: "#2E86AB",
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home-outline" size={size} color={color} />
-          ),
+          title: 'Home',
+          tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color={color} />,
         }}
       />
+
       <Tabs.Screen
         name="search"
         options={{
-          title: "Search",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="search-outline" size={size} color={color} />
-          ),
+          title: 'Search',
+          headerShown: true,
+          tabBarIcon: ({ color }) => <Feather name="search" size={24} color={color} />,
         }}
       />
+
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: 'Saved',
+          headerShown: true,
+          tabBarIcon: ({ color }) => <EvilIcons name="heart" size={27} color={color} />,
+        }}
+      />
+
       <Tabs.Screen
         name="inbox"
         options={{
-          title: "Inbox",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="chatbubble-outline" size={size} color={color} />
-          ),
+          title: 'Inbox',
+          headerShown: true,
+          tabBarIcon: ({ color }) => <Ionicons name="chatbubbles-outline" size={24} color={color} />,
         }}
       />
+
       <Tabs.Screen
         name="profile"
         options={{
-          title: "Profile",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person-outline" size={size} color={color} />
-          ),
+          title: 'Profile',
+          headerShown: true,
+          tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color={color} />,
         }}
       />
     </Tabs>
