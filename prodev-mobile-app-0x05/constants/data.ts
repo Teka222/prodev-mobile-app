4ad51bd0a8d8e8556c9c4ef942ef5d76cf6a5089
Tekano Meso <tekanonetflorist01@gmail.com>
2025-09-13 19:42:42 +0200

    Create data.ts

---
+++ prodev-mobile-app-0x05/constants/data.ts
@@ -0,0 +1,14 @@
+import { PropertyListingProps } from "@/interfaces";
+
+const SAMPLE_DATA: PropertyListingProps[] = [
+  {
+    propertyName: "Villa Arrciffee Beach House",
+    location: { street: "123 Ocean Drive", city: "Miami", country: "USA" },
+    rate: 4.76,
+    currency: "$",
+    amount: 620,
+    favorite: false,
+  },
+  {
+    propertyName: "Sunset Paradise Villa",
+    locat
